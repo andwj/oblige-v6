88bbb46cc87095b79fde3f0a50871fe999439c0d
Andrew Apted <ajapted@users.sf.net>
2015-03-06 11:43:00 +1100

    Bump version to 6.20

--- gui/main.h
+++ gui/main.h
@@ -23,8 +23,8 @@
 
 #define OBLIGE_TITLE  "OBLIGE Level Maker"
 
-#define OBLIGE_VERSION  "6.10"
-#define OBLIGE_HEX_VER  0x610
+#define OBLIGE_VERSION  "6.20"
+#define OBLIGE_HEX_VER  0x620
 
 
 #define CONFIG_FILENAME   "CONFIG.txt"
